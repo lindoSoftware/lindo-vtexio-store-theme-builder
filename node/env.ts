78b8e1c3b12b704eca85a229e70d3383cb403b19
Ruben Gumiel <benchu.mza@gmail.com>
2025-10-29 01:08:21 -0300

    delete

--- node/env.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  STRAPI_GRAPHQL_URL: 'https://ioadmin.lindo.la/graphql/',
-  GITHUB_TOKEN: 'memmefdf',
-  GIT_API_URL: 'https://api.github.com',
-  GIT_OWNER: 'lindoSoftware',
-  GIT_REPOSITORY: 'lindo-vtexio-store-theme',
-  GIT_BRANCH: 'testing',
-}

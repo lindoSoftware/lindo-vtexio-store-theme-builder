da5052f6e82faeef1275035a7524c5ebcf8e7575
Ruben Gumiel <benchu.mza@gmail.com>
2025-12-02 18:17:38 -0300

    refactor

---
+++ node/middlewares/strategies/layout/MultipleImageSelectorProcessor.ts
@@ -0,0 +1,135 @@
+import {
+  ImageItem,
+  ImageSelector,
+  MultipleImageSelectorBlock,
+} from '../../../typings/homepage-response'
+import { isWithinDateRange } from '../../../utils/isWithinDateRange'
+import { BlockProcessor } from './BlockProcessor'
+import env from '../../../env'
+
+export class MultipleImageSelectorProcessor extends BlockProcessor<MultipleImageSelectorBlock> {
+  process(section: MultipleImageSelectorBlock): void {
+    const validSelectors = section.imageSelectors.filter((s) =>
+      isWithinDateRange(s.beginning, s.expiration)
+    )
+
+    if (validSelectors.length === 0) return
+
+    validSelectors.forEach((selector, index) => {
+      this.processSelector(selector, index + 1)
+    })
+  }
+
+  private processSelector(selector: ImageSelector, index: number): void {
+    if (selector.title) {
+      this.createTitleBlock(selector.title, index)
+    }
+
+    this.createSliderBlock(selector, index)
+  }
+
+  private createTitleBlock(title: string, index: number): void {
+    const titleBlock = `flex-layout.row#image-selector-title-${index}`
+    const richTextSelector = `rich-text#image-selector-title-${index}`
+
+    this.addToHome(titleBlock)
+
+    this.createBlock(titleBlock, {
+      blockName: titleBlock,
+      children: [richTextSelector],
+    })
+
+    this.createBlock(richTextSelector, {
+      blockName: richTextSelector,
+      props: {
+        text: title,
+        blockClass: 'image-selector-title',
+      },
+    })
+  }
+
+  private createSliderBlock(selector: ImageSelector, index: number): void {
+    const selectorRow = `flex-layout.row#image-selector-slider-${index}`
+    const sliderSelector = `slider-layout#image-selector-${index}`
+
+    this.addToHome(selectorRow)
+
+    this.createBlock(selectorRow, {
+      blockName: selectorRow,
+      children: [sliderSelector],
+    })
+
+    const imageColumns = this.createImageColumns(selector.images)
+
+    this.createBlock(sliderSelector, {
+      blockName: sliderSelector,
+      props: {
+        infinity: true,
+        showPaginationDots: 'never',
+        itemsPerPage: {
+          desktop: selector.itemsPerPageDesktop,
+          tablet: selector.itemsPerPageTablet,
+          phone: selector.itemsPerPageMobile,
+        },
+      },
+      children: imageColumns,
+    })
+  }
+
+  private createImageColumns(images: ImageItem[]): string[] {
+    return images.map((image, i) => {
+      const col = `flex-layout.col#item${i + 1}`
+      const link = `link#item${i + 1}`
+
+      this.createBlock(col, {
+        blockName: col,
+        props: {
+          horizontalAlign: 'center',
+          verticalAlign: 'middle',
+        },
+        children: [link],
+      })
+
+      const linkChildren = this.createLinkChildren(image, i)
+
+      this.createBlock(link, {
+        blockName: link,
+        children: linkChildren,
+        props: {
+          href: image.link ?? '',
+        },
+      })
+
+      return col
+    })
+  }
+
+  private createLinkChildren(image: ImageItem, index: number): string[] {
+    const children: string[] = []
+
+    if (image.image) {
+      const imageBlock = `image#img${index + 1}`
+      this.createBlock(imageBlock, {
+        blockName: imageBlock,
+        props: {
+          src: env.STRAPI_URL + image.image.url,
+        },
+      })
+      children.push(imageBlock)
+    }
+
+    if (image.text) {
+      const textBlock = `rich-text#title${index + 1}`
+      this.createBlock(textBlock, {
+        blockName: textBlock,
+        props: {
+          text: image.text,
+          blockClass: 'imageText',
+        },
+      })
+      children.push(textBlock)
+    }
+
+    return children
+  }
+}
---
+++ node/middlewares/strategies/layout/SliderBlockProcessor.ts
@@ -0,0 +1,44 @@
+import { SliderBlock } from '../../../typings/homepage-response'
+import { isWithinDateRange } from '../../../utils/isWithinDateRange'
+import { BlockProcessor } from './BlockProcessor'
+import env from '../../../env'
+
+export class SliderBlockProcessor extends BlockProcessor<SliderBlock> {
+  private counter = 0
+
+  process(section: SliderBlock): void {
+    const validBanners = section.banners.filter((b) =>
+      isWithinDateRange(b.beginning, b.expiration)
+    )
+
+    if (validBanners.length === 0) return
+
+    this.counter++
+    const bannerRow = `flex-layout.row#banner-${this.counter}`
+    const imageList = `list-context.image-list#banner-${this.counter}`
+
+    this.addToHome(bannerRow)
+
+    this.createBlock(bannerRow, {
+      blockName: bannerRow,
+      children: [imageList],
+    })
+
+    this.createBlock(imageList, {
+      blockName: imageList,
+      children: ['slider-layout#slider'],
+      props: {
+        height: section.height,
+        preload: section.preload,
+        images: validBanners.map((b) => ({
+          image: env.STRAPI_URL + b.desktopImage.url,
+          mobileImage: env.STRAPI_URL + b.mobileImage.url,
+          link: {
+            url: b.link ?? '',
+            openNewTab: false,
+          },
+        })),
+      },
+    })
+  }
+}
